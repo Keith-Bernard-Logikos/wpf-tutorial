2dd563fe95cdb7265afb08bd4c1d0525cebfefd0
Keith Bernard <kbernard@logikos.com>
2023-08-16 14:51:18 -0400

    Tutorial 4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFTutorial
 {
@@ -20,9 +7,29 @@ namespace WPFTutorial
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool running = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            btnToggleRun.Content = "Run";
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                // stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                // run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            running = !running;
         }
     }
 }
