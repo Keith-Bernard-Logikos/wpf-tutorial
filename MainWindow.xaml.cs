b368c3b8b5c930c72db212a97b545c607b88ac34
Keith Bernard <kbernard@logikos.com>
2023-08-16 15:14:47 -0400

    Tutorial 5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,24 +12,6 @@ namespace WPFTutorial
         public MainWindow()
         {
             InitializeComponent();
-            btnToggleRun.Content = "Run";
-        }
-
-        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
-        {
-            if (running)
-            {
-                // stop
-                tbStatus.Text = "Stopped";
-                btnToggleRun.Content = "Run";
-            }
-            else
-            {
-                // run
-                tbStatus.Text = "Running";
-                btnToggleRun.Content = "Stop";
-            }
-            running = !running;
         }
     }
 }
